00ce4db52fe421eb8baf1d015459b8544a21d7d3
joshnevius <joshnevius92@gmail.com>
2016-07-17 03:31:15 -0400

    Added two GET routes in TasksController for editing and creating new tasks.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,8 +3,20 @@ class TasksController < ApplicationController
   get '/tasks' do
     redirect_if_not_logged_in
     @tasks = Task.all
-    erb :'/tasks/index'
+    erb :'tasks/index'
   end
 
-  
+  get '/tasks/new' do
+    redirect_if_not_logged_in
+    @error_message = params[:error]
+    erb :'tasks/new'
+  end
+
+  get '/tasks/:id/edit' do
+    redirect_if_not_logged_in
+    @error_message = params[:error]
+    @task = Task.find(params[:id])
+    erb :'tasks/edit'
+  end
+
 end
