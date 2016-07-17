22f2ad6bf277d711ebcc83939ba26b5cd03cf61b
joshnevius <joshnevius92@gmail.com>
2016-07-17 18:20:14 -0400

    Added :email to Users DB

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160717020702) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email"
     t.string "username"
     t.string "password_digest"
   end
