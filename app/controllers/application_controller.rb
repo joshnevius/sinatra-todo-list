286d167cb14856b2e27848395f99a8699c249099
joshnevius <joshnevius92@gmail.com>
2016-07-16 22:33:11 -0400

    Enabled sessions and added GET request for index page in ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,16 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :session
+    set :secret_session, 'secret_todo_list'
+  end
 
+  get '/' do
+    erb :index
+  end
+  
 
 end
