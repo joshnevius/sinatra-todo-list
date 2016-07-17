19bfcf91b040e39c5bc71c5a6621d69d3f824268
joshnevius <joshnevius92@gmail.com>
2016-07-17 03:16:06 -0400

    Edited/added get request for /lists/new to provide error message.

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,7 +1,14 @@
 class ListsController < ApplicationController
 
+  get '/lists' do
+    redirect_if_not_logged_in
+    @lists = List.all
+    erb :'lists/index'
+  end
+
   get '/lists/new' do
     redirect_if_not_logged_in
+    @error_message = params[:error]
     erb :'/lists/new'
   end
 
@@ -14,20 +21,17 @@ class ListsController < ApplicationController
 
   get '/lists/:id/edit' do
     redirect_if_not_logged_in
-    @list = List.find_by_id(params[:id])
+    @list = List.find(params[:id])
     erb :'/lists/edit'
   end
 
-  patch "/lists/:id" do
+  get "/lists/:id" do
+    redirect_if_not_logged_in
     @list = List.find_by_id(params[:id])
     @list.name = params[:name]
     @list.save
     redirect '/tasks'
   end
 
-  get '/lists' do
-    redirect_if_not_logged_in
-    @lists = List.all
-    erb :'lists/index'
-  end
+
 end
