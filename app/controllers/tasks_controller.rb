e5653466eda7669f8efc3583a49ffc3614e4ba65
joshnevius <joshnevius92@gmail.com>
2016-07-17 03:35:37 -0400

    Added POST route for /tasks/:id and added List.all to previous routes to show all lists and tasks when request is made

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,6 +9,7 @@ class TasksController < ApplicationController
   get '/tasks/new' do
     redirect_if_not_logged_in
     @error_message = params[:error]
+    @lists = List.all
     erb :'tasks/new'
   end
 
@@ -16,7 +17,19 @@ class TasksController < ApplicationController
     redirect_if_not_logged_in
     @error_message = params[:error]
     @task = Task.find(params[:id])
+    @lists = List.all
     erb :'tasks/edit'
   end
+
+  post '/tasks/:id' do
+    redirect_if_not_logged_in
+    @task = Task.find(params[:id])
+    unless Task.valid_params?(params)
+      redirect '/tasks/#{@task.id}/edit?error=invalid task'
+    end
+    @task.update(params.select{|l| l=="name"})
+    redirect '/tasks/#{@task.id}'
+  end
+
 
 end
