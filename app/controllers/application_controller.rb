58d336322e29023e1aadbee83b090586ae2dd992
joshnevius <joshnevius92@gmail.com>
2016-07-16 23:39:13 -0400

    Added helper methods to Application Controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,15 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
-  
+
+  helpers do
+    
+    def is_logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
 
 end
