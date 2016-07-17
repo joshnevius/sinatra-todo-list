25e79476ed4b754cf31d618f7dd328610c6ad476
joshnevius <joshnevius92@gmail.com>
2016-07-16 23:58:21 -0400

    Added POST request for /signup page and GET request for login page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,5 +22,6 @@ class ApplicationController < Sinatra::Base
     def current_user
       User.find(session[:user_id])
     end
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,30 @@
 class UsersController < ApplicationController
 
   get '/signup' do
-    erb :'/users/create_user'
+    if !session[:user_id]
+      erb :'/users/create_user'
+    else
+      redirect to '/lists'
+    end
   end
-  
+
+  post '/signup' do
+    if params[:username].empty? || params[:password].empty?
+      redirect to '/signup'
+    else 
+      @user = User.new(username: params[:username], password: params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect "/lists"
+    end
+  end
+
+  get '/login' do
+    if !session[:user_id]
+      redirect '/lists'
+    else
+      erb :'/users/login'
+    end
+  end
+
 end
